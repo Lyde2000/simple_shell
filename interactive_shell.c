2963640d9788f23dde67b9f59e3905ed2b462fec
Lyde2000 <lateleginia@gmail.com>
2023-10-15 14:13:07 -0700

    shell

---
+++ interactive_shell.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+/**
+ * interactive_shell - function that handles commands from command line
+ * Return: void
+ */
+void interactive_shell(void)
+{
+char *line;
+char **args;
+int status = -1;
+while (1)
+{
+prompt_user();
+line = read_line();
+args = split_line(line);
+status = exec_cmd(args);
+free(line);
+free(args);
+if (status >= 0)
+{
+exit(status);
+}
+} while (status == -1);
+}
