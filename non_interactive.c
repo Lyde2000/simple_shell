f8d91c87221422b7449185edb66b28db3ca93625
Lyde2000 <lateleginia@gmail.com>
2023-10-15 14:22:50 -0700

    inter

---
+++ non_interactive.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+ * non_interactive_shell - Function for non-interactive mode execution
+ *
+ * Return: void
+ */
+
+void non_interactive_shell(void)
+{
+char *line;
+char **args;
+int status = -1;
+
+while (1)
+{
+line = read_stream();
+args = split_line(line);
+status = exec_cmd(args);
+/* avoid memory leaks */
+free(line);
+free(args);
+
+/* exit with status */
+if (status >= 0)
+{
+exit(status);
+}
+} while (status == -1);
+}
